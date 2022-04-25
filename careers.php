2d6c2acc87ea2799d691f84cbaa7335f096ba364
Nishant Pandey <nishantpandey2106@gmail.com>
2022-04-25 18:27:34 +0530

    career page

--- careers.php
+++ careers.php
@@ -15,5 +15,52 @@
             transition: none;
         }
     </style>
+     <!-- Contact Start -->
+     <div class="contact mt-125">
+            <div class="container">
+                <div class="section-header">
+                    <!-- <p>Get In Touch</p> -->
+                    <h2>Get In Touch For Any Query</h2>
+                </div>
+                <div class="row align-items-center">
+                    <div class="col-12">
+                        <div class="contact-form">
+                            <div id="success"></div>
+                            <form name="sentMessage" id="contactForm" novalidate="novalidate">
+                                <div class="control-group">
+                                    <input type="text" class="form-control" id="name" placeholder="Your Name" required="required" data-validation-required-message="Please enter your name" />
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                                <div class="control-group">
+                                    <input type="text" class="form-control" id="mobile" placeholder="Your Mobile No." required="required" data-validation-required-message="Please enter your mobile number" />
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                                <div class="control-group">
+                                    <input type="email" class="form-control" id="email" placeholder="Your Email" required="required" data-validation-required-message="Please enter your email" />
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                                <div class="control-group">
+                                    <input type="file" class="form-control" id="resume"  required="required" data-validation-required-message="Please enter your Resume" />
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                                <div class="control-group">
+                                    <input type="text" class="form-control" id="qualification" placeholder="Your Qualification" required="required" data-validation-required-message="Please enter your qualification" />
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                                <div class="control-group">
+                                    <textarea class="form-control" id="experience" placeholder="Description of Experience/Job Position" required="required" data-validation-required-message="Enter Your Experience/Job"></textarea>
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                                <div>
+                                    <button class="btn" type="submit" id="sendMessageButton">Send Message</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Contact End -->
+    <?php require("footer.php"); ?>
 </body>
 </html>
